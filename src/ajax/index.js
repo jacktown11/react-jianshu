8cf279b150bd5ee689acf3912842afe98ebd29be
jacktown11 <jikai_tang@163.com>
2019-01-26 15:34:35 +0800

    fix: axios interceptors for development

--- src/ajax/index.js
+++ src/ajax/index.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
-// if (process.env.NODE_ENV === 'development') {
-// }
 axios.interceptors.request.use(function(config) {
-  config.url = config.url.replace('/api', '/react-jianshu/api') + '.json';
+  let url = config.url;
+  if (process.env.NODE_ENV === 'production') {
+    url = url.replace('/api', '/react-jianshu/api');
+  }
+  config.url = url + '.json';
   return config;
 });
