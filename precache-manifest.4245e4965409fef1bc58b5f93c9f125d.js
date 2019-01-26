91b25e5a41f66ea8ab11d6b4fbf9220ba7aad86d
jacktown11 <jikai_tang@163.com>
2019-01-26 13:00:57 +0800

    Updates

---
+++ precache-manifest.4245e4965409fef1bc58b5f93c9f125d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f951edf38d67fd77b28",
+    "url": "/react-jianshu/static/js/runtime~main.0f951edf.js"
+  },
+  {
+    "revision": "5b82d1f6e0b35a2acd43",
+    "url": "/react-jianshu/static/js/main.5b82d1f6.chunk.js"
+  },
+  {
+    "revision": "5adb86b05047e821ffca",
+    "url": "/react-jianshu/static/js/1.5adb86b0.chunk.js"
+  },
+  {
+    "revision": "2cf7bd1f41d25d9fb2e3151872f89e61",
+    "url": "/react-jianshu/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/react-jianshu/precache-manifest.4245e4965409fef1bc58b5f93c9f125d.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/react-jianshu/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
